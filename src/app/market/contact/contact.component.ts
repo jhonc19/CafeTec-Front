e9c06ac2c3142b0bf0ad9d4b631256b51699a996
Ronny Chupillon <cronnyjoel2@gmail.com>
2021-12-03 02:20:28 -0500

    actualizacion contact

--- src/app/market/contact/contact.component.ts
+++ src/app/market/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { faChevronRight, faPhoneAlt, faFax } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight, faPhoneAlt, faMapMarkerAlt  } from '@fortawesome/free-solid-svg-icons';
+import {  faFacebookSquare, faTwitter, faYoutubeSquare, faInstagram, faSkype} from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
@@ -9,8 +10,12 @@ export class ContactComponent implements OnInit {
   
   faChevronRight = faChevronRight;
   faPhoneAlt  = faPhoneAlt ;
-  faFax = faFax;
-  
+  faMapMarkerAlt=faMapMarkerAlt;
+  faFacebookSquare=faFacebookSquare;
+  faTwitter = faTwitter;
+  faYoutubeSquare=faYoutubeSquare;
+  faInstagram=faInstagram;
+  faSkype=faSkype;
 
   constructor() { }
 
